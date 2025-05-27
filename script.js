e30b5fbf65fbb532c4868513719b6d69daa571f4
Praveen Kumar S <praveen040102@gmail.com>
2025-05-27 18:15:05 +0530

    Added some JS code

--- script.js
+++ script.js
@@ -2,8 +2,22 @@
 let js = "boring";
 //js = "amazing";
 
+/*
 if (js === "amazing") {
   alert("Hello World");
 } else {
   alert("Hello India");
 }
+*/
+
+console.log(48 + 22 + 19 + 1 - 10);
+
+// Assignment -1
+
+let country = "India";
+let continent = "Asia";
+let population = "100+";
+
+console.log(
+  `I live in ${country} which is in ${continent} with over ${population} million people.`
+);
